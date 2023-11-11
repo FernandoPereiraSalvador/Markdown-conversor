8cb9176f158af6d8145205bff375f8abaef22c1d
FernandoPereiraSalvador <fernandoplanet9000@gmail.com>
2023-11-11 22:41:21 +0100

    Cliente
    Se ha añadido una carpeta temporal para que los archivos luego se suban al servidor
    
    Se han añadido funciones para convertir el archivo a html y pdf

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import os
+import subprocess
+import tempfile
+import time
+import traceback
 import flet
 import markdown
 import requests
@@ -11,6 +16,8 @@ from flet import (
     Dropdown,
     FilePickerUploadFile
 )
+
+carpeta_temporal = tempfile.mkdtemp()
 
 
 class App:
@@ -53,7 +60,12 @@ class App:
                 upload_url = self.page.get_upload_url(f.name, 60)
                 upload_file = FilePickerUploadFile(f.name, upload_url)
                 self.file_picker.upload([upload_file])
-                # requests.post("http://localhost:8000/upload", files={'file': e.data})
+                print(carpeta_temporal)
+                archivo_en_carpeta = os.path.join(carpeta_temporal, f.name)
+                print(r"Ruta del archivo:", archivo_en_carpeta)
+                time.sleep(1)
+                with open(archivo_en_carpeta, 'rb') as file:
+                    requests.post("http://localhost:8000/upload", files={'file': file})
                 # Enviar una solicitud al servidor para obtener el contenido del archivo
                 response = requests.get(f'http://localhost:8000/upload/{f.name}')
                 print(response.text)
@@ -62,16 +74,74 @@ class App:
 
     def submit_clicked(self, e):
         if self.dropdown.value == "html":
-            for archivo in self.archivos:
-                self.convert_md_to_html(archivo)
+            self.convert_md_to_html()
+        elif self.dropdown.value == "pdf":
+            self.convert_md_to_pdf()
 
-    def convert_md_to_html(self, archivo):
-        # Enviar una solicitud al servidor para obtener el contenido del archivo
-        print("Convertir")
+    def convert_md_to_html(self):
+        # Crear la carpeta 'downloads' si no existe
+        carpeta_descargas = 'downloads'
+        os.makedirs(carpeta_descargas, exist_ok=True)
+
+        for archivo in self.archivos:
+            # Obtener el contenido del archivo Markdown desde el servidor
+            response = requests.get(f'http://localhost:8000/upload/{archivo}')
+
+            if response.status_code == 200:
+                # Convertir el contenido de Markdown a HTML
+                contenido_markdown = response.text
+                contenido_html = markdown.markdown(contenido_markdown)
+
+                # Guardar el contenido HTML en un archivo en la carpeta 'downloads'
+                archivo_html = os.path.join(carpeta_descargas, f'{archivo.replace(".md", ".html")}')
+                with open(archivo_html, 'w', encoding='utf-8') as archivo_salida:
+                    archivo_salida.write(contenido_html)
+
+                print(f"Contenido HTML para {archivo} guardado en {archivo_html}")
+            else:
+                print(f"Error al obtener el archivo {archivo}. Estado de la respuesta: {response.status_code}")
+
+    def convert_md_to_pdf(self):
+        for archivo in self.archivos:
+            # Obtener el contenido del archivo Markdown desde el servidor
+            url_archivo = f'http://localhost:8000/upload/{archivo}'
+            response = requests.get(url_archivo)
+
+            if response.status_code == 200:
+                # Crea un archivo temporal
+                temp = tempfile.NamedTemporaryFile(delete=False)
+
+                try:
+                    # Escribe el contenido de Markdown en el archivo temporal
+                    with open(temp.name, 'w', encoding='utf-8') as f:
+                        f.write(response.text)
+
+                    # Convertir el archivo de Markdown a PDF usando Pandoc
+                    archivo_pdf = os.path.join("downloads", f'{archivo.replace(".md", ".pdf")}')
+                    self.convert_md_to_pdf_with_pandoc(temp.name, archivo_pdf)
+                    print(f"Contenido PDF para {archivo} guardado en {archivo_pdf}")
+                except Exception as e:
+                    traceback.print_exc()
+                    print(f"Error al convertir {archivo} a PDF. Detalles del error:\n{e}")
+                finally:
+                    # Cierra el archivo temporal antes de intentar eliminarlo
+                    temp.close()
+                    # Elimina el archivo temporal
+                    os.unlink(temp.name)
+            else:
+                traceback.print_exc()
+                print(
+                    f"Error al obtener el archivo {archivo} desde {url_archivo}. Estado de la respuesta: {response.status_code}")
+
+    def convert_md_to_pdf_with_pandoc(self, input_file, output_file):
+        try:
+            subprocess.run(["pandoc", input_file, "-o", output_file])
+        except Exception as e:
+            raise Exception(f"Error al convertir Markdown a PDF usando Pandoc. Detalles del error:\n{e}")
 
 
 def main(page: Page):
     app = App(page)
 
 
-flet.app(target=main, view=flet.WEB_BROWSER, upload_dir="upload")
+flet.app(target=main, view=flet.WEB_BROWSER, upload_dir=carpeta_temporal)
